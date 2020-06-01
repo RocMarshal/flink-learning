40af4e52d93e33e6ac1f183d2f72794e0f61e404
RocMarshal <flinker@126.com>
2020-06-01 22:40:52 +0800

    添加调试信息

--- flink-learning-window/src/main/java/com/zhisheng/window/Main.java
+++ flink-learning-window/src/main/java/com/zhisheng/window/Main.java
@@ -4,10 +4,14 @@ import com.zhisheng.common.utils.ExecutionEnvUtil;
 import com.zhisheng.function.LineSplitter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.streaming.api.windowing.assigners.ProcessingTimeSessionWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+
+import java.util.Random;
 
 import static com.zhisheng.constant.WindowConstant.HOST_NAME;
 import static com.zhisheng.constant.WindowConstant.PORT;
@@ -24,9 +28,23 @@ public class Main {
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         //如果不指定时间的话，默认是 ProcessingTime，但是如果指定为事件事件的话，需要事件中带有时间或者添加时间水印
-//        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
         ParameterTool parameterTool = ExecutionEnvUtil.PARAMETER_TOOL;
-        DataStreamSource<String> data = env.socketTextStream(parameterTool.get(HOST_NAME), parameterTool.getInt(PORT));
+        //DataStreamSource<String> data = env.socketTextStream(parameterTool.get(HOST_NAME), parameterTool.getInt(PORT));
+        DataStreamSource<String> data = env.addSource(new SourceFunction<String>() {
+            @Override
+            public void run(SourceContext<String> ctx) throws Exception {
+                while (true){
+                    Thread.sleep(300L);
+                    ctx.collect(String.valueOf(new Random().nextInt(9200)));
+                }
+            }
+
+            @Override
+            public void cancel() {
+
+            }
+        });
 
         //基于时间窗口
 /*        data.flatMap(new LineSplitter())
@@ -36,11 +54,11 @@ public class Main {
                 .print();*/
 
         //基于滑动时间窗口
-/*        data.flatMap(new LineSplitter())
+        data.flatMap(new LineSplitter())
                 .keyBy(1)
                 .timeWindow(Time.seconds(60), Time.seconds(30))
                 .sum(0)
-                .print();*/
+                .print();
 
 
         //基于事件数量窗口
@@ -60,11 +78,11 @@ public class Main {
 
 
         //基于会话时间窗口
-        data.flatMap(new LineSplitter())
+        /*data.flatMap(new LineSplitter())
                 .keyBy(1)
                 .window(ProcessingTimeSessionWindows.withGap(Time.seconds(5))) //表示如果 5s 内没出现数据则认为超出会话时长，然后计算这个窗口的和
                 .sum(0)
-                .print();
+                .print();*/
 
         env.execute("zhisheng —— flink window example");
     }
